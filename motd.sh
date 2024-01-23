56f69b404e304718df412df8999c15a5e14df597
Ditzzy <hello@zyy.sh>
2024-01-23 08:52:00 +0700

    Update motd.sh

--- motd.sh
+++ motd.sh
@@ -3,7 +3,7 @@ clear
 BIN_DATE='/bin/date'
 BIN_FIGLET='/usr/bin/figlet'
 BIN_HOSTNAME='/bin/hostname'
-BIN_IP='/sbin/ip'
+BIN_IP='/bin/hostname -i'
 BIN_GREP='/bin/grep'
 BIN_SED='/bin/sed'
 BIN_UPTIME='/usr/bin/uptime'
@@ -26,14 +26,11 @@ COLOR_YELLOW='\033[1;33m'
 COLOR_RED='\033[0;31m'
 COLOR_WHITE='\033[1;37m'
 COLOR_DEFAULT='\033[0m'
-
 echo -e "
-
-${COLOR_LIGHT_GREEN}$(${BIN_FIGLET} -ckw 80 -f slant "NextSekai VPS")
+${COLOR_LIGHT_GREEN}$(${BIN_FIGLET} -f smblock "NextSekai VPS")
 ${COLOR_WHITE}Date and time             = ${COLOR_LIGHT_BLUE}$(${BIN_DATE})
 ${COLOR_WHITE}Hostname                  = ${COLOR_LIGHT_BLUE}$(${BIN_HOSTNAME})
-${COLOR_WHITE}Global IP Addresses       = ${COLOR_LIGHT_BLUE}$(${BIN_IP} addr show eth0 2>/dev/null | ${BIN_GREP} 'inet ' | ${BIN_SED} -e 's/.*inet \([^ ]*\)\/.*/\1/' | ${BIN_SED} ':a;N;$!ba;s/\n/, /g')
-${COLOR_WHITE}VPN IP Address            = ${COLOR_LIGHT_BLUE}$(${BIN_IP} addr show tun0 2>/dev/null | ${BIN_GREP} 'inet ' | awk -F" " '{print $2}' | ${BIN_SED} ':a;N;$!ba;s/\n/, /g')
+${COLOR_WHITE}Global IP Addresses       = ${COLOR_LIGHT_BLUE}$(${BIN_IP})
 ${COLOR_WHITE}Uptime                    = ${COLOR_LIGHT_BLUE}$(${BIN_UPTIME} | ${BIN_CUT} -d "," -f 1 | xargs)
 ${COLOR_WHITE}Load avg.                 = ${COLOR_LIGHT_BLUE}$(${BIN_UPTIME} | ${BIN_GREP} -o -e "load averages\?.*" | ${BIN_SED} -e "s/load averages\?: //")
 ${COLOR_WHITE}Release                   = ${COLOR_LIGHT_BLUE}$(${BIN_LSB_RELEASE} -d --short)
@@ -42,10 +39,16 @@ ${COLOR_WHITE}CPU Usage (Core)          = ${COLOR_LIGHT_BLUE}$(echo $(${BIN_PS} 
 ${COLOR_WHITE}Memory         Used/Total = ${COLOR_LIGHT_BLUE}$(${BIN_FREE} -m | ${BIN_HEAD} -n 2 | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $3'})/$(${BIN_FREE} -m | ${BIN_HEAD} -n 2 | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $2'})MB
 ${COLOR_WHITE}Swap           Used/Total = ${COLOR_LIGHT_BLUE}$(${BIN_FREE} -m | tail -n 1 | ${BIN_AWK} {'print $3'})/$(${BIN_FREE} -m | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $2'})MB
 ${COLOR_WHITE}Disk(/)        Used/Total = ${COLOR_LIGHT_BLUE}$(${BIN_DF} -h / | tail -n 1 | ${BIN_AWK} {'print $3'})/$(${BIN_DF} -h / | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $2'})
-
-${COLOR_WHITE}Jangan lupa follow ${COLOR_RED}@ditzzsxz_ ${COLOR_WHITE}yaaa... arigatou and enjoyy your vps~~
 ${COLOR_DEFAULT}
-"
+===========[ ${COLOR_LIGHT_BLUE}Terms of Service${COLOR_DEFAULT} ]===============
+${COLOR_RED}✘ DILARANG MINING
+${COLOR_RED}✘ DILARANG DIJADIKAN SERVER VPN
+${COLOR_RED}✘ DILARANG UNTUK MENGGUNAKAN KEGIATAN ILLEGAL 
+${COLOR_RED}✘ DILARANG UNTUK BRUTE FORCE
+${COLOR_RED}✘ And other illegal activities...${COLOR_DEFAULT}
+==============================================
+${COLOR_WHITE}Jangan lupa follow ${COLOR_RED}@ditzzsxz_ ${COLOR_WHITE}yaaa... arigatou and enjoyy your vps~~
+${COLOR_DEFAULT}"
 
 unset BIN_DATE BIN_FIGLET BIN_HOSTNAME BIN_IP BIN_GREP BIN_SED BIN_UPTIME BIN_CAT BIN_LSB_RELEASE BIN_UNAME BIN_FREE BIN_HEAD BIN_TAIL BIN_DF BIN_AWK BIN_BC BIN_PS
 unset COLOR_LIGHT_GREEN COLOR_LIGHT_BLUE COLOR_YELLOW COLOR_RED COLOR_WHITE COLOR_DEFAULT
