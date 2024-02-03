77b3ac73718a73b764f0f1beedc56e31c9e40ca5
Ditzzy <hello@zyy.sh>
2024-02-03 21:32:37 +0700

    Update motd.sh

--- motd.sh
+++ motd.sh
@@ -8,6 +8,11 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
     source ~/.zshrc
     sudo su
 fi
+mb_to_gb() {
+    local mb=$1
+    local gb=$(echo "scale=2; $mb / 1024" | bc)
+    echo "$gb"
+}
 
 BIN_DATE='/bin/date'
 BIN_FIGLET='/usr/bin/figlet'
@@ -35,20 +40,55 @@ COLOR_YELLOW='\033[1;33m'
 COLOR_RED='\033[0;31m'
 COLOR_WHITE='\033[1;37m'
 COLOR_DEFAULT='\033[0m'
+# colors
+W="\e[0;39m"
+G="\e[1;32m"
+R="\e[1;31m"
+dim="\e[2m"
+undim="\e[0m"
+# get load averages
+IFS=" " read LOAD1 LOAD5 LOAD15 <<<$(awk '{ print $1,$2,$3 }' /proc/loadavg)
+# get free memory
+IFS=" " read USED AVAIL TOTAL <<<$(free -htm | awk '/Mem/ { print $3,$7,$2 }')
+# get processes
+PROCESS=$(ps -eo user=|sort|uniq -c | awk '{ print $2 " " $1 }')
+PROCESS_ALL=$(echo "$PROCESS"| awk {'print $2'} | awk '{ SUM += $1} END { print SUM }')
+PROCESS_ROOT=$(echo "$PROCESS" | awk '/root/ { print $2}')
+PROCESS_USER=$(echo "$PROCESS" | awk '!/root/ { SUM += $2} END { print SUM }')
+# get processors
+PROCESSOR_NAME=$(awk -F": " '/model name/ { print $2 }' /proc/cpuinfo | head -1)
+PROCESSOR_COUNT=$(grep -ioPc 'processor\t:' /proc/cpuinfo)
+# Memory usage
+memory_used_mb=$(${BIN_FREE} -m | ${BIN_HEAD} -n 2 | ${BIN_TAIL} -n 1 | ${BIN_AWK} '{print $3}')
+memory_total_mb=$(${BIN_FREE} -m | ${BIN_HEAD} -n 2 | ${BIN_TAIL} -n 1 | ${BIN_AWK} '{print $2}')
+memory_used_gb=$(mb_to_gb $memory_used_mb)
+memory_total_gb=$(mb_to_gb $memory_total_mb)
+
+# Swap usage
+swap_used_mb=$(${BIN_FREE} -m | ${BIN_TAIL} -n 1 | ${BIN_AWK} '{print $3}')
+swap_total_mb=$(${BIN_FREE} -m | ${BIN_TAIL} -n 1 | ${BIN_AWK} '{print $2}')
+swap_used_gb=$(mb_to_gb $swap_used_mb)
+swap_total_gb=$(mb_to_gb $swap_total_mb)
+
+# Disk usage
+disk_used_mb=$(${BIN_DF} -m / | ${BIN_TAIL} -n 1 | ${BIN_AWK} '{print $3}')
+disk_total_mb=$(${BIN_DF} -m / | ${BIN_TAIL} -n 1 | ${BIN_AWK} '{print $2}')
+disk_used_gb=$(mb_to_gb $disk_used_mb)
+disk_total_gb=$(mb_to_gb $disk_total_mb)
 echo -e "
-${COLOR_LIGHT_GREEN}$(${BIN_FIGLET} -f smblock "NextSekai VPS")
-${COLOR_WHITE}Date and time             = ${COLOR_LIGHT_BLUE}$(${BIN_DATE})
-${COLOR_WHITE}Hostname                  = ${COLOR_LIGHT_BLUE}$(${BIN_HOSTNAME})
-${COLOR_WHITE}Global IP Addresses       = ${COLOR_LIGHT_BLUE}$(${BIN_IP})
-${COLOR_WHITE}Uptime                    = ${COLOR_LIGHT_BLUE}$(${BIN_UPTIME} | ${BIN_CUT} -d "," -f 1 | xargs)
-${COLOR_WHITE}Load avg.                 = ${COLOR_LIGHT_BLUE}$(${BIN_UPTIME} | ${BIN_GREP} -o -e "load averages\?.*" | ${BIN_SED} -e "s/load averages\?: //")
-${COLOR_WHITE}Release                   = ${COLOR_LIGHT_BLUE}$(${BIN_LSB_RELEASE} -d --short)
-${COLOR_WHITE}Kernel                    = ${COLOR_LIGHT_BLUE}$(${BIN_UNAME} -ro)
-${COLOR_WHITE}CPU Usage (Core)          = ${COLOR_LIGHT_BLUE}$(echo $(${BIN_PS} -eo pcpu | ${BIN_AWK} 'NR>1' | ${BIN_AWK} '{tot=tot+$1} END {print tot}') / $(${BIN_CAT} /proc/cpuinfo | ${BIN_GREP} -c processor) | ${BIN_BC} )%
-${COLOR_WHITE}Memory         Used/Total = ${COLOR_LIGHT_BLUE}$(${BIN_FREE} -m | ${BIN_HEAD} -n 2 | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $3'})/$(${BIN_FREE} -m | ${BIN_HEAD} -n 2 | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $2'})MB
-${COLOR_WHITE}Swap           Used/Total = ${COLOR_LIGHT_BLUE}$(${BIN_FREE} -m | tail -n 1 | ${BIN_AWK} {'print $3'})/$(${BIN_FREE} -m | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $2'})MB
-${COLOR_WHITE}Disk(/)        Used/Total = ${COLOR_LIGHT_BLUE}$(${BIN_DF} -h / | tail -n 1 | ${BIN_AWK} {'print $3'})/$(${BIN_DF} -h / | ${BIN_TAIL} -n 1 | ${BIN_AWK} {'print $2'})
-${COLOR_DEFAULT}
+${COLOR_LIGHT_GREEN}$(${BIN_FIGLET} -f smblock "NextSekai VPS")${COLOR_DEFAULT}
+==========[ ${COLOR_LIGHT_BLUE}System Information${COLOR_DEFAULT} ]=============
+$W  Hostname$dim····$undim: $W${HOSTNAME}
+$W  Distro$dim······$undim: $W$(grep "PRETTY_NAME" /etc/*release | cut -d "=" -f 2- | sed 's/"//g')
+$W  Kernel$dim······$undim: $W$(uname -sr)
+$W  Uptime$dim······$undim: $W$(uptime -p)
+$W  Load$dim········$undim: $G$LOAD1$W (1m), $G$LOAD5$W (5m), $G$LOAD15$W (15m)
+$W  Processes$dim···$undim: $G$PROCESS_ROOT$W (root), $G$PROCESS_USER$W (user), $G$PROCESS_ALL$W (total)
+$W  CPU$dim·········$undim: $W$PROCESSOR_NAME ($G$PROCESSOR_COUNT$W vCPU)
+$W  Memory$dim······$undim: $G$USED$W used, $G$AVAIL$W avail, $G$TOTAL$W total
+$W  Swap$dim········$undim: $G${swap_used_gb}${COLOR_DEFAULT}GB/$G${swap_total_gb}${COLOR_DEFAULT}GB
+$W  Disk(/)$dim·····$undim: ${G}${disk_used_gb}${W}GB/$G${disk_total_gb}${W}GB
+
 ===========[ ${COLOR_LIGHT_BLUE}Terms of Service${COLOR_DEFAULT} ]===============
 ${COLOR_RED}✘ DILARANG MINING
 ${COLOR_RED}✘ DILARANG DIJADIKAN SERVER VPN
@@ -56,8 +96,11 @@ ${COLOR_RED}✘ DILARANG UNTUK MENGGUNAKAN KEGIATAN ILLEGAL
 ${COLOR_RED}✘ DILARANG UNTUK BRUTE FORCE
 ${COLOR_RED}✘ And other illegal activities...${COLOR_DEFAULT}
 ==============================================
-${COLOR_WHITE}Jangan lupa follow ${COLOR_RED}@ditzzsxz_ ${COLOR_WHITE}yaaa... arigatou and enjoyy your vps~~
-${COLOR_DEFAULT}"
+${COLOR_WHITE}Jangan lupa follow ${COLOR_RED}@ditzzsxz_ ${COLOR_WHITE}yaaa... arigatou and enjoyy your vps~~"
+# config
+max_usage=90
+bar_width=50
+
 
 unset BIN_DATE BIN_FIGLET BIN_HOSTNAME BIN_IP BIN_GREP BIN_SED BIN_UPTIME BIN_CAT BIN_LSB_RELEASE BIN_UNAME BIN_FREE BIN_HEAD BIN_TAIL BIN_DF BIN_AWK BIN_BC BIN_PS
 unset COLOR_LIGHT_GREEN COLOR_LIGHT_BLUE COLOR_YELLOW COLOR_RED COLOR_WHITE COLOR_DEFAULT
